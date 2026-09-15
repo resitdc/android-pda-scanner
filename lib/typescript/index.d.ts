598449fbb4483da020bedf92991a83d432638c0a
Restu Dwi Cahyo <restu@resitdc.id>
2026-09-15 22:20:56 +0700

    recommit after added gitignore

--- lib/typescript/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { EmitterSubscription } from "react-native";
-export declare function addBarcodeListener(callback: (barcode: string) => void): EmitterSubscription;
-//# sourceMappingURL=index.d.ts.map
